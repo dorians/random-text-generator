29f30627bd89a258ad2df61c3fbcd13e7e36e8e0
Dorian Sarnowski <dorian.sarnowski@allegro.pl>
2013-11-07 20:33:57 +0100

    Fix executable file

--- bin/random-text-generator.rb
+++ bin/random-text-generator.rb
@@ -1,6 +1,10 @@
-require '../src/single_word_generator.rb'
-require '../src/link_generator.rb'
-require '../src/text_generator.rb'
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'src')
+
+require 'single_word_generator.rb'
+require 'link_generator.rb'
+require 'text_generator.rb'
 
 wordGenerator = SingleWordGenerator.new()
 linkGenerator = LinkGenerator.new(wordGenerator)
